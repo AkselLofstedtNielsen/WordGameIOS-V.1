fdb440c78caebf96b0fbb6635784440902b1b60a
Aksellofstedtnielsen <aksel98@outlook.com>
2022-12-31 00:06:22 +0100

    Initial Commit

---
+++ WordGameIOS-V.1/WordGameIOS_V_1App.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameIOS_V_1App.swift
+//  WordGameIOS-V.1
+//
+//  Created by Aksel Nielsen on 2022-12-31.
+//
+
+import SwiftUI
+
+@main
+struct WordGameIOS_V_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
